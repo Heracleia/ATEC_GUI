e80236ba5b9b0489a23b87b82c53afbb7f89902e
MikeMpapa <mikepapakostas@gmail.com>
2017-09-30 14:53:55 -0500

    Atec Main

---
+++ CogniApp.py
@@ -0,0 +1,26 @@
+from kivy.app import App
+from kivy.lang import Builder
+from kivy.uix.screenmanager import ScreenManager, Screen, FadeTransition
+from kivy.uix.widget import Widget
+from kivy.uix.floatlayout import FloatLayout
+
+                  
+
+class GamesA(Screen):
+    pass
+
+class MainScreen(Screen):
+    pass
+
+class ScreenManagement(ScreenManager):
+    pass
+
+presentation = Builder.load_file("CogniApp.kv")
+
+class CogniApp(App):
+    def build(self):
+        return presentation
+
+
+if __name__ == '__main__':
+    CogniApp().run()
