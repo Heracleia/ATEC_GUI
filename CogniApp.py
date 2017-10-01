35f804e68e7b88514224cc771ae64d6cfc31e29e
MikeMpapa <mikepapakostas@gmail.com>
2017-10-01 17:24:20 -0500

    update

--- CogniApp.py
+++ CogniApp.py
@@ -102,10 +102,9 @@ Builder.load_string('''
 
 
 <Cmenu1>
-    size_hint: None, None
-    size: 120, 250
-    pos: (5, 50)
-    padding: 5
+    size_hint: 0.4, 0.6
+    pos_hint: {"right":0.65,"top":0.495}
+    padding: 10
     background_color: .2, .9, 1, .7
     #wanna have some fun? set this to 'data/images/image-loading.gif'
     background_image: 'atlas://data/images/defaulttheme/button_pressed'
@@ -120,25 +119,12 @@ Builder.load_string('''
                 BoxLayout:
                     orientation: 'vertical'
                     ListButton:
-                        text: 'Hello'
+                        text: 'March Slow'
                         on_release: root.menu_selected
                     ListButton:
-                        text: 'World'
+                        text: 'March Slow'
                         on_release: root.menu_selected
-                    ListButton:
-                        text: 'hows'
-                        #'>'image
-                        btn_img: 'atlas://data/images/defaulttheme/tree_closed'
-                        on_release: root.menu_selected
-                    ListButton:
-                        text: 'it'
-                        on_release: root.menu_selected
-                    ListButton:
-                        text: 'going'
-                        on_release: root.menu_selected
-                    ListButton:
-                        text: 'peace'
-                        on_release: root.menu_selected
+                       
                 # end root menu
                 #sub-menu
                 BoxLayout:
@@ -171,8 +157,61 @@ Builder.load_string('''
 
 
 
-
 <Cmenu2>
+    size_hint: 0.4, 0.6
+    pos_hint: {"right":0.65,"top":0.495}
+    padding: 10
+    background_color: .2, .9, 1, .7
+    #wanna have some fun? set this to 'data/images/image-loading.gif'
+    background_image: 'atlas://data/images/defaulttheme/button_pressed'
+    orientation: 'vertical'
+    BoxLayout:
+        padding: 5
+        ScrollView:
+            BoxLayout:
+                size_hint: None, 1
+                width: root.width * 2 - 40
+                #root menu add/edit items here to show them in root menu
+                BoxLayout:
+                    orientation: 'vertical'
+                    ListButton:
+                        text: 'March Slow'
+                        on_release: root.menu_selected
+                    ListButton:
+                        text: 'March Slow'
+                        on_release: root.menu_selected
+                       
+                # end root menu
+                #sub-menu
+                BoxLayout:
+                    ListButton:
+                        # go back(root menu) button
+                        text: '<'
+                        size_hint: (.15, 1)
+                        on_release: root.menu_selected
+                    BoxLayout:
+                        orientation: 'vertical'
+                        ListButton:
+                            text: 'The'
+                            on_release: root.menu_selected
+                        ListButton:
+                            text: 'revolving'
+                            on_release: root.menu_selected
+                        ListButton:
+                            text: 'door'
+                            on_release: root.menu_selected
+                        ListButton:
+                            text: 'hits'
+                            on_release: root.menu_selected
+                        ListButton:
+                            text: 'every'
+                            on_release: root.menu_selected
+                        ListButton:
+                            text: 'one'
+                            on_release: root.menu_selected
+                #end sub-menu
+
+<Cmenu3>
     size_hint: None, None
     size: 120, 250
     pos: (5, 50)
@@ -193,7 +232,116 @@ Builder.load_string('''
                     ListButton:
                         text: 'Hello'
                         on_release: root.menu_selected
-                 
+
+<Cmenu4>
+    size_hint: None, None
+    size: 120, 250
+    pos: (5, 50)
+    padding: 5
+    background_color: .2, .9, 1, .7
+    #wanna have some fun? set this to 'data/images/image-loading.gif'
+    background_image: 'atlas://data/images/defaulttheme/button_pressed'
+    orientation: 'vertical'
+    BoxLayout:
+        padding: 5
+        ScrollView:
+            BoxLayout:
+                size_hint: None, 1
+                width: root.width * 2 - 40
+                #root menu add/edit items here to show them in root menu
+                BoxLayout:
+                    orientation: 'vertical'
+                    ListButton:
+                        text: 'Hello'
+                        on_release: root.menu_selected
+
+<Cmenu5>
+    size_hint: None, None
+    size: 120, 250
+    pos: (5, 50)
+    padding: 5
+    background_color: .2, .9, 1, .7
+    #wanna have some fun? set this to 'data/images/image-loading.gif'
+    background_image: 'atlas://data/images/defaulttheme/button_pressed'
+    orientation: 'vertical'
+    BoxLayout:
+        padding: 5
+        ScrollView:
+            BoxLayout:
+                size_hint: None, 1
+                width: root.width * 2 - 40
+                #root menu add/edit items here to show them in root menu
+                BoxLayout:
+                    orientation: 'vertical'
+                    ListButton:
+                        text: 'Hello'
+                        on_release: root.menu_selected
+
+<Cmenu6>
+    size_hint: None, None
+    size: 120, 250
+    pos: (5, 50)
+    padding: 5
+    background_color: .2, .9, 1, .7
+    #wanna have some fun? set this to 'data/images/image-loading.gif'
+    background_image: 'atlas://data/images/defaulttheme/button_pressed'
+    orientation: 'vertical'
+    BoxLayout:
+        padding: 5
+        ScrollView:
+            BoxLayout:
+                size_hint: None, 1
+                width: root.width * 2 - 40
+                #root menu add/edit items here to show them in root menu
+                BoxLayout:
+                    orientation: 'vertical'
+                    ListButton:
+                        text: 'Hello'
+                        on_release: root.menu_selected
+
+<Cmenu7>
+    size_hint: None, None
+    size: 120, 250
+    pos: (5, 50)
+    padding: 5
+    background_color: .2, .9, 1, .7
+    #wanna have some fun? set this to 'data/images/image-loading.gif'
+    background_image: 'atlas://data/images/defaulttheme/button_pressed'
+    orientation: 'vertical'
+    BoxLayout:
+        padding: 5
+        ScrollView:
+            BoxLayout:
+                size_hint: None, 1
+                width: root.width * 2 - 40
+                #root menu add/edit items here to show them in root menu
+                BoxLayout:
+                    orientation: 'vertical'
+                    ListButton:
+                        text: 'Hello'
+                        on_release: root.menu_selected
+
+<Cmenu8>
+    size_hint: None, None
+    size: 120, 250
+    pos: (5, 50)
+    padding: 5
+    background_color: .2, .9, 1, .7
+    #wanna have some fun? set this to 'data/images/image-loading.gif'
+    background_image: 'atlas://data/images/defaulttheme/button_pressed'
+    orientation: 'vertical'
+    BoxLayout:
+        padding: 5
+        ScrollView:
+            BoxLayout:
+                size_hint: None, 1
+                width: root.width * 2 - 40
+                #root menu add/edit items here to show them in root menu
+                BoxLayout:
+                    orientation: 'vertical'
+                    ListButton:
+                        text: 'Hello'
+                        on_release: root.menu_selected                 
 ''')
 
 
@@ -222,6 +370,144 @@ class Cmenu1(Bubble):
 
 
 class Cmenu2(Bubble):
+
+    def menu_selected(self, *l):
+        if l[0].text == 'hows':
+            # move to sub menu
+            Animation(scroll_x = 1, d=.5 ).start(l[0].parent.parent.parent)
+            #l[0].parent.parent.parent change this and everything relative to something non-relative if you want-to make the menu more extensible
+        elif l[0].text == '<':
+            # move back to root menu
+            Animation(scroll_x = 0, d=.5 ).start(l[0].parent.parent.parent)
+        else:
+            #fade out animation
+            (r, g, b, a) = self.parent.context_menu.background_color
+
+            def on_anim_complete(*l):
+                self.parent.context_menu.background_color = (r, g, b, a)
+                self.parent.remove_widget(self.parent.context_menu)
+
+            anim = Animation(background_color = (0, 0, 0, 0), d=.1 )
+            anim.start(self.parent.context_menu)
+            anim.bind(on_complete = on_anim_complete)
+            print l[0].text + ' selected'
+
+class Cmenu3(Bubble):
+
+    def menu_selected(self, *l):
+        if l[0].text == 'hows':
+            # move to sub menu
+            Animation(scroll_x = 1, d=.5 ).start(l[0].parent.parent.parent)
+            #l[0].parent.parent.parent change this and everything relative to something non-relative if you want-to make the menu more extensible
+        elif l[0].text == '<':
+            # move back to root menu
+            Animation(scroll_x = 0, d=.5 ).start(l[0].parent.parent.parent)
+        else:
+            #fade out animation
+            (r, g, b, a) = self.parent.context_menu.background_color
+
+            def on_anim_complete(*l):
+                self.parent.context_menu.background_color = (r, g, b, a)
+                self.parent.remove_widget(self.parent.context_menu)
+
+            anim = Animation(background_color = (0, 0, 0, 0), d=.1 )
+            anim.start(self.parent.context_menu)
+            anim.bind(on_complete = on_anim_complete)
+            print l[0].text + ' selected'
+
+class Cmenu4(Bubble):
+
+    def menu_selected(self, *l):
+        if l[0].text == 'hows':
+            # move to sub menu
+            Animation(scroll_x = 1, d=.5 ).start(l[0].parent.parent.parent)
+            #l[0].parent.parent.parent change this and everything relative to something non-relative if you want-to make the menu more extensible
+        elif l[0].text == '<':
+            # move back to root menu
+            Animation(scroll_x = 0, d=.5 ).start(l[0].parent.parent.parent)
+        else:
+            #fade out animation
+            (r, g, b, a) = self.parent.context_menu.background_color
+
+            def on_anim_complete(*l):
+                self.parent.context_menu.background_color = (r, g, b, a)
+                self.parent.remove_widget(self.parent.context_menu)
+
+            anim = Animation(background_color = (0, 0, 0, 0), d=.1 )
+            anim.start(self.parent.context_menu)
+            anim.bind(on_complete = on_anim_complete)
+            print l[0].text + ' selected'
+
+class Cmenu5(Bubble):
+
+    def menu_selected(self, *l):
+        if l[0].text == 'hows':
+            # move to sub menu
+            Animation(scroll_x = 1, d=.5 ).start(l[0].parent.parent.parent)
+            #l[0].parent.parent.parent change this and everything relative to something non-relative if you want-to make the menu more extensible
+        elif l[0].text == '<':
+            # move back to root menu
+            Animation(scroll_x = 0, d=.5 ).start(l[0].parent.parent.parent)
+        else:
+            #fade out animation
+            (r, g, b, a) = self.parent.context_menu.background_color
+
+            def on_anim_complete(*l):
+                self.parent.context_menu.background_color = (r, g, b, a)
+                self.parent.remove_widget(self.parent.context_menu)
+
+            anim = Animation(background_color = (0, 0, 0, 0), d=.1 )
+            anim.start(self.parent.context_menu)
+            anim.bind(on_complete = on_anim_complete)
+            print l[0].text + ' selected'
+
+class Cmenu6(Bubble):
+
+    def menu_selected(self, *l):
+        if l[0].text == 'hows':
+            # move to sub menu
+            Animation(scroll_x = 1, d=.5 ).start(l[0].parent.parent.parent)
+            #l[0].parent.parent.parent change this and everything relative to something non-relative if you want-to make the menu more extensible
+        elif l[0].text == '<':
+            # move back to root menu
+            Animation(scroll_x = 0, d=.5 ).start(l[0].parent.parent.parent)
+        else:
+            #fade out animation
+            (r, g, b, a) = self.parent.context_menu.background_color
+
+            def on_anim_complete(*l):
+                self.parent.context_menu.background_color = (r, g, b, a)
+                self.parent.remove_widget(self.parent.context_menu)
+
+            anim = Animation(background_color = (0, 0, 0, 0), d=.1 )
+            anim.start(self.parent.context_menu)
+            anim.bind(on_complete = on_anim_complete)
+            print l[0].text + ' selected'
+
+class Cmenu7(Bubble):
+
+    def menu_selected(self, *l):
+        if l[0].text == 'hows':
+            # move to sub menu
+            Animation(scroll_x = 1, d=.5 ).start(l[0].parent.parent.parent)
+            #l[0].parent.parent.parent change this and everything relative to something non-relative if you want-to make the menu more extensible
+        elif l[0].text == '<':
+            # move back to root menu
+            Animation(scroll_x = 0, d=.5 ).start(l[0].parent.parent.parent)
+        else:
+            #fade out animation
+            (r, g, b, a) = self.parent.context_menu.background_color
+
+            def on_anim_complete(*l):
+                self.parent.context_menu.background_color = (r, g, b, a)
+                self.parent.remove_widget(self.parent.context_menu)
+
+            anim = Animation(background_color = (0, 0, 0, 0), d=.1 )
+            anim.start(self.parent.context_menu)
+            anim.bind(on_complete = on_anim_complete)
+            print l[0].text + ' selected'
+
+class Cmenu8(Bubble):
 
     def menu_selected(self, *l):
         if l[0].text == 'hows':
@@ -293,20 +579,25 @@ class MainMenu(FloatLayout) :
     def add_menu(self, obj, submenu_id,*l):
         if  hasattr(self, 'context_menu'):
                 self.remove_widget(self.context_menu)
-        if submenu_id == 5:
-            
+        if submenu_id == 1:
             self.context_menu = Cmenu1()
-            self.remove_widget(self.context_menu)
-            self.add_widget(self.context_menu)
-            self.context_menu.pos = obj.pos[0]+ obj.width, obj.pos[1]
-        elif submenu_id == 6:
-            if not hasattr(self, 'context_menu'):
-                print "sfdsdfsdfs"
+        elif submenu_id == 2:
             self.context_menu = Cmenu2()
-            self.remove_widget(self.context_menu)
-            self.add_widget(self.context_menu)
-            self.context_menu.pos = obj.pos[0]+ obj.width, obj.pos[1]
-
+        elif submenu_id == 3:
+            self.context_menu = Cmenu3()
+        elif submenu_id == 4:
+            self.context_menu = Cmenu4()
+        elif submenu_id == 5:
+            self.context_menu = Cmenu5()
+        elif submenu_id == 6:
+            self.context_menu = Cmenu6()
+        elif submenu_id == 7:
+            self.context_menu = Cmenu7()
+        elif submenu_id == 8:
+            self.context_menu = Cmenu8()
+        self.add_widget(self.context_menu)
+        self.context_menu.pos = obj.pos[0]+ obj.width, obj.pos[1]
+       
     def menu_selected(self, *l):
         if l[0].text == 'Cross Body Game':
             # move to sub menu
@@ -354,17 +645,14 @@ class MainPage(FloatLayout):
         #allow kids to get touch
         if super(MainPage, self).on_touch_down(*l):
             return True
+        self.clear_widgets()
+        for child in self.children:
+            child.canvas.ask_update()
+            for c in child.children:
+                c.canvas.ask_update()
+
         # remove menu when touched and menu exists
-        if hasattr(self, 'context_menu'):
-            self.remove_widget(self.context_menu)
-
-    def add_menu(self, obj, *l):
-        if not hasattr(self, 'context_menu'):
-            self.context_menu = MainMenu()
-        self.remove_widget(self.context_menu)
-        self.add_widget(self.context_menu)
-        self.context_menu.pos = obj.pos[0]+ obj.width, obj.pos[1]
-
+        
 
 
 class MyApp(App):
